4802d79bc47d95ddb9495aee0d4bac62fc8706f6
Bheki Freeman <bhekisisadlamini05@gmail.com>
2023-09-18 03:49:08 +0200

    Refactoring of the table users in the database

--- employee.py
+++ employee.py
@@ -2,6 +2,7 @@ import sqlite3
 from getpass import getpass
 con = sqlite3.connect('main.db')
 cur = con.cursor()
+from datetime import date
 
 
 class Employee():
@@ -17,17 +18,33 @@ class Employee():
 
     def registerEmployee(self):
         print('Welcome to our simple registration')
-        username = input('Enter username: ')
-        userpass = getpass('Type Password: ')
-        sql = "CREATE TABLE IF NOT EXISTS user(id integer primary key autoincrement not null, username, userpassword)"
+        empcode = input('Enter Emp Code [BF100]: ')
+        empname = input('Enter username: ')
+        gender = input('Enter Gender: ')
+        job = input('Enter job title: ')
+        phone = int(input('Enter Phone number [26876294516]: '))
+        deprt = input('Enter department: ')
+        datejoined = date.today()
+        emppass = getpass('Type Password: ')
+        self.setValues(empcode, empname, gender, phone, job, deprt, datejoined)
+        sql = "CREATE TABLE IF NOT EXISTS users(\
+            empcode text primary key,\
+            empname,\
+            gender,\
+            phone int,\
+            jobtitle text,\
+            department text,\
+            datejoined date,\
+            userpassword)"
         cur.execute(sql)
-        data = [(None, username, userpass)]
-        cur.executemany("INSERT INTO user VALUES(?, ?, ?)", data)
+        data = [(self.empcode, self.empname, self.gender, self.phone, self.jobtitle,\
+                self.department, self.datejoined, emppass)]
+        cur.executemany("INSERT INTO users VALUES(?, ?, ?, ?, ?, ?, ?, ?)", data)
         con.commit()
-        print('Registration Successful')
+        print('Registration Successfull')
 
     def loginEmployee(self, user_name, userpass):
-        sql = f"SELECT username, userpassword FROM user WHERE username='{user_name}' AND  userpassword='{userpass}'"
+        sql = f"SELECT empname, userpassword FROM users WHERE empname='{user_name}' AND  userpassword='{userpass}'"
         cur.execute(sql)
         if not cur.fetchone():
             return False
